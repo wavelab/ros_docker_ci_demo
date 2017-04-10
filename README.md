c411522714aa29f00c02d43714be0952e106599b
Michael Smart <michael.smart87@gmail.com>
2017-04-10 14:59:47 -0400

    Update demo to match updates to ros_docker_ci

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 This demo repository exists to illustrate the usage of the `ros_docker_ci`
 repository found [here](https://github.com/wavelab/ros_docker_ci).
 
-TODO: Update once CI/submodule setup is complete.
+If copying this repository's `.travis.yml` file to use ros_docker_ci, be sure
+to modify the contained areas marked `MODIFY_ME`.
 
 
 
--- scripts/run-ci-tests.bash
+++ scripts/run-ci-tests.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
-
 set -e  # exit on first error
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
-source /ros_docker_ci_demo/scripts/identify_environment.bash
+# get UBUNTU_CODENAME, ROS_DISTRO, REPO_DIR, CATKIN_DIR
+source $SCRIPT_DIR/identify_environment.bash
 
 sudo apt-get update
 
