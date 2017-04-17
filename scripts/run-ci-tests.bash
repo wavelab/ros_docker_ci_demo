cb9312d83f14e9103cba4623c725f9091208e5a1
michaelhsmart <michael.smart87@gmail.com>
2017-04-17 12:55:16 -0400

    Update run-ci-tests.bash

--- scripts/run-ci-tests.bash
+++ scripts/run-ci-tests.bash
@@ -3,9 +3,9 @@ set -e  # exit on first error
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source /opt/ros/${ROS_DISTRO}/setup.bash
 # get UBUNTU_CODENAME, ROS_DISTRO, REPO_DIR, CATKIN_DIR
 source $SCRIPT_DIR/identify_environment.bash
+source /opt/ros/${ROS_DISTRO}/setup.bash
 
 sudo apt-get update
 
